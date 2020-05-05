3521bf249fe27f5c9b86031a9844695820df4479
jurusteve <jstevemu17@gmail.com>
2020-05-04 22:21:19 -0400

    refactored launches context and components

--- src/components/Launches/Launches.js
+++ src/components/Launches/Launches.js
@@ -1,28 +1,45 @@
-import React from "react"
+import React, { useEffect } from "react"
 import RocketLaunch from "./RocketLaunch"
 import styled from "styled-components"
 import Filter from "../Filter/Filter"
 import { LaunchProvider, LaunchConsumer } from "../../context/LaunchesContext"
+import useLaunchData from "../../hooks/useLaunchData"
 
 const Launches = () => {
+  const { state, filters, fetchLaunches } = useLaunchData()
+  useEffect(() => {
+    fetchLaunches()
+  }, [])
+
   return (
-    <LaunchProvider>
-      <LaunchConsumer>
-        {({ launches }) => (
-          <AllLaunches>
-            <h1>All Space X Rocket Launches</h1>
-            <Filter launches={launches} />
-            <ul className="launch-list">
-              {launches &&
-                launches.map((launch, i) => {
-                  return <RocketLaunch launch={launch} key={i} />
-                })}
-            </ul>
-          </AllLaunches>
-        )}
-      </LaunchConsumer>
-    </LaunchProvider>
+    <AllLaunches>
+      <Filter filters={filters} />
+      <ul className="launch-list">
+        {state.launches.length &&
+          state.launches.map((launch, i) => {
+            return <RocketLaunch key={i} launch={launch}></RocketLaunch>
+          })}
+      </ul>
+    </AllLaunches>
   )
+  // return (
+  //   <LaunchProvider>
+  //     <LaunchConsumer>
+  //       {({ launches }) => (
+  //         <AllLaunches>
+  //           <h1>All Space X Rocket Launches</h1>
+  //           <Filter launches={launches} />
+  //           <ul className="launch-list">
+  //             {launches &&
+  //               launches.map((launch, i) => {
+  //                 return <RocketLaunch launch={launch} key={i} />
+  //               })}
+  //           </ul>
+  //         </AllLaunches>
+  //       )}
+  //     </LaunchConsumer>
+  //   </LaunchProvider>
+  // )
 }
 
 const AllLaunches = styled.section`
