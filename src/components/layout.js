a35128af41f4f6e7e77c380cbdf92171135b8723
jurusteve <jstevemu17@gmail.com>
2020-06-05 18:53:29 -0400

    fixed typo

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import { Header, Footer } from "../components"
+import { Header, Footer } from "."
 import { GlobalStyles, ContainerWrapper } from "../elements"
 
 const Layout = ({ children }) => {
